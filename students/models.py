103f419c7f537ac550c5143d34898eea3c2996ab
stevenrentas <smrentas@gmail.com>
2023-04-15 15:25:03 -0500

    Finish HW#10

--- students/models.py
+++ students/models.py
@@ -1,5 +1,5 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 
 # Create your models here.
 
@@ -7,14 +7,14 @@ class Student(models.Model):
     first_name = models.CharField(max_length=60)
     last_name = models.CharField(max_length=60)
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE, primary_key=False)
-    
+
     def __str__(self) -> str:
         return self.first_name + " " + self.last_name
-    
+
 class Course(models.Model):
     department = models.CharField(max_length=60)
     number = models.CharField(max_length=3)
-    students = models.ManyToManyField(Student)
+    students = models.ManyToManyField(Student, related_name='student_courses')
 
     def __str__(self) -> str:
         return self.department + "-" + self.number
