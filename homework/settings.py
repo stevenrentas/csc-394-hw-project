02efa90dde06ab404ee1a45e7f5e49bb41c3e679
stevenrentas <smrentas@gmail.com>
2023-04-15 15:22:52 -0500

    Moving on to last step 1.5

--- homework/settings.py
+++ homework/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 import environ
 # Initialise environment variables
@@ -59,7 +60,7 @@ ROOT_URLCONF = 'homework.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'homework/templates'), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
